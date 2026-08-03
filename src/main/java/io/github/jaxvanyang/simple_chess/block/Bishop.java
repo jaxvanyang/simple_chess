0494ee58d1290581527892dea1f9fe01daf3d48b
Spagles <106791090+Spagles@users.noreply.github.com>
2026-08-03 23:33:16 +1000

    drop dead imports left over from copying the block scaffolds

--- src/main/java/io/github/jaxvanyang/simple_chess/block/Bishop.java
+++ src/main/java/io/github/jaxvanyang/simple_chess/block/Bishop.java
@@ -1,16 +1,9 @@
 package io.github.jaxvanyang.simple_chess.block;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
