99e28927c6029b568e8059f970aa9b7283cc0b71
Jax Young <jaxvanyang@gmail.com>
2026-08-18 18:54:38 +0800

    feat: allow switch pieces if main hand has only one piece

--- src/main/java/io/github/jaxvanyang/simple_chess/Chess.java
+++ src/main/java/io/github/jaxvanyang/simple_chess/Chess.java
@@ -2,9 +2,12 @@ package io.github.jaxvanyang.simple_chess;
 
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.resources.ResourceLocation;
+import com.mojang.logging.LogUtils;
+import org.slf4j.Logger;
 
 public class Chess implements ModInitializer {
     public static final String MOD_ID = "simple_chess";
+    public static final Logger LOGGER = LogUtils.getLogger();
 
     @Override
     public void onInitialize() {
--- src/main/java/io/github/jaxvanyang/simple_chess/block/GrabBlock.java
+++ src/main/java/io/github/jaxvanyang/simple_chess/block/GrabBlock.java
@@ -1,9 +1,15 @@
 package io.github.jaxvanyang.simple_chess.block;
 
+import io.github.jaxvanyang.simple_chess.Chess;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
@@ -12,13 +18,26 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 
 public class GrabBlock extends Block {
+    public static final TagKey<Block> PIECE_BLOCK = TagKey.create(Registries.BLOCK, new ResourceLocation("simple_chess", "chess_piece"));
+    public static final TagKey<Item> PIECE_ITEM = TagKey.create(Registries.ITEM, new ResourceLocation("simple_chess", "chess_piece"));
+
     public GrabBlock(Properties properties) {
         super(properties);
     }
 
+    private enum Action {
+        GRAB, CAPTURE,
+    }
+
     @Override
     public InteractionResult use(BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand interactionHand, BlockHitResult blockHitResult) {
-        if (!player.getMainHandItem().isEmpty()) {
+        ItemStack itemStack = player.getMainHandItem();
+        Action action;
+        if (interactionHand == InteractionHand.MAIN_HAND && itemStack.is(PIECE_ITEM) && itemStack.getCount() == 1 && blockState.is(PIECE_BLOCK)) {
+            action = Action.CAPTURE;
+        } else if (itemStack.isEmpty()) {
+            action = Action.GRAB;
+        } else {
             return super.use(blockState, level, blockPos, player, interactionHand, blockHitResult);
         }
 
@@ -26,8 +45,24 @@ public class GrabBlock extends Block {
             return InteractionResult.SUCCESS;
         }
 
-        level.setBlock(blockPos, Blocks.AIR.defaultBlockState(), Block.UPDATE_ALL);
-        player.setItemInHand(InteractionHand.MAIN_HAND, new ItemStack(this));
+        if (action == Action.CAPTURE) {
+            if (!(itemStack.getItem() instanceof BlockItem blockItem)) {
+                Chess.LOGGER.error("expected block item");
+                return InteractionResult.FAIL;
+            }
+
+            Block block = blockItem.getBlock();
+            BlockState state = block.defaultBlockState();
+            if (block instanceof DirectionBlock) {
+                state = state.setValue(DirectionBlock.FACING, player.getDirection());
+            }
+
+            level.setBlock(blockPos, state, Block.UPDATE_ALL);
+            player.setItemInHand(InteractionHand.MAIN_HAND, new ItemStack(this));
+        } else {
+            level.setBlock(blockPos, Blocks.AIR.defaultBlockState(), Block.UPDATE_ALL);
+            player.setItemInHand(InteractionHand.MAIN_HAND, new ItemStack(this));
+        }
 
         return InteractionResult.SUCCESS;
     }
